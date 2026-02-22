270acd3fb9f82f9e94dafa2ab31eb5f59d19bf6a
kamani jha <kamanijha10@gmail.com>
2026-02-22 17:22:22 +0530

    Kadane's Algorithm

--- Subarray/subArray.java
+++ Subarray/subArray.java
@@ -32,4 +32,24 @@ public class subArray {
         }
         return maxSum;
     }
+
+    // max subarray sum Kadane's Algorithm
+
+    static int maxSubarraySum(int[] arr) {
+        int res = arr[0];
+  
+      
+        for (int i = 0; i < arr.length; i++) {
+            int currSum = 0;
+      
+            
+            for (int j = i; j < arr.length; j++) {
+                currSum = currSum + arr[j];
+              
+                
+                res = Math.max(res, currSum);
+            }
+        }
+        return res;
+    }
 }
